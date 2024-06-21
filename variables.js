2848082933f4103d006d07d338495cc2942b5ab3
mjerta <maarten.postma1000@gmail.com>
2024-06-21 13:31:37 +0200

    Add variables and console log

--- variables.js
+++ variables.js
@@ -8,37 +8,58 @@ const start = "Let's go!";
 // 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 
+const name = "Maarten";
+
 
 // ==========================================
 // 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 
+const colorEyes = "Green";
 
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let age = 32;
 
 // ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
+
+let profession = "Developer";
 
 
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 
+const shoeSize = "44";
+
 
 // ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
 // ==========================================
+
+let hasPartner = true;
 
 
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let amountChildren = 2;
+
 
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
 // ==========================================
+
+console.log(start);
+console.log(name);
+console.log(colorEyes);
+console.log(age);
+console.log(profession);
+console.log(shoeSize);
+console.log(hasPartner);
+console.log(amountChildren);
